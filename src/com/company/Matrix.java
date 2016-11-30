b2855d5387c40ae66be81c47a3387e78681732ca
Dival Banerjee <bandiv2011@gmail.com>
2016-11-30 10:48:41 -0500

    Initial commit

---
+++ src/com/company/Matrix.java
@@ -0,0 +1,21 @@
+package com.company;
+
+/**
+ * Created by bandi on 11/30/2016.
+ */
+public class Matrix {
+    private double[][] myNumbers;
+    private int myRows;
+    private int myColumns;
+
+    public Matrix(int Rows, int Columns){
+        this.myRows = Rows;
+        this.myColumns = Columns;
+    }
+
+    public void generateMatrix(){
+        for(int i = 0; i < this.myRows; i++){
+            
+        }
+    }
+}
